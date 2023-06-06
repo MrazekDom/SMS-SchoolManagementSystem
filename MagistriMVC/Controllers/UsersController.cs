674ede4e14da083afcfb741ad73ccfb14546c62f
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-06-06 20:54:22 +0200

    Add http GET and POST method for view "Create"

--- MagistriMVC/Controllers/UsersController.cs
+++ MagistriMVC/Controllers/UsersController.cs
@@ -1,10 +1,11 @@
 ﻿using MagistriMVC.Models;
+using MagistriMVC.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagistriMVC.Controllers {
     public class UsersController : Controller {
-        private UserManager<AppUser> userManager;
+        private UserManager<AppUser> userManager;       //built in servicka
 
         public UsersController(UserManager<AppUser> userManager) {
             this.userManager = userManager;
@@ -12,6 +13,29 @@ namespace MagistriMVC.Controllers {
 
         public IActionResult Index() {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]      //metoda pro vytvoreni usera
+        public async Task<IActionResult> Create(UserVM user) {
+            if (ModelState.IsValid) {
+                AppUser appUser = new AppUser {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+                //pokus o zápis nového uživatele do databáze
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
         }
     }
 }
