6fec583aab3c9c52b448e399408b6ecbf3a1d61d
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-06-06 21:16:01 +0200

    Add "Edit" view for Users, add httpPost Edit + Errors for users

--- MagistriMVC/Controllers/UsersController.cs
+++ MagistriMVC/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MagistriMVC.Controllers {
     public class UsersController : Controller {
         private UserManager<AppUser> userManager;       //built in servicka
+        private IPasswordHasher<AppUser> passwordHasher;        //pro zahasovani a overeni hesla v databazi
 
         public UsersController(UserManager<AppUser> userManager) {
             this.userManager = userManager;
@@ -37,5 +38,45 @@ namespace MagistriMVC.Controllers {
             }
             return View(user);
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id) {
+            AppUser userToEdit = await userManager.FindByIdAsync(id);
+            if (userToEdit == null)
+                return View("NotFound");
+            else
+                return View(userToEdit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string email, string password) {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null) {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password)) {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
+        private void Errors(IdentityResult result) {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+
+
     }
 }
