91c0d085a5ffb347fd5e008bedd0c9a599986a00
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-06-08 18:06:52 +0200

    Add RolesControllers, add Roles button to NavBar

---
+++ SchoolManagementSystem/Controllers/RolesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementSystem.Controllers {
+    public class RolesController : Controller {
+        private RoleManager<IdentityRole> roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager) {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index() { 
+            return View();
+        }
+
+        private void Errors(IdentityResult result) {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+    }
+}
