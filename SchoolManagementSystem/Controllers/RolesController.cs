9820f8cfb7ddae73820d6ba60139cc86ea4f9b0f
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-06-08 18:11:34 +0200

    Add Index view for Roles

--- SchoolManagementSystem/Controllers/RolesController.cs
+++ SchoolManagementSystem/Controllers/RolesController.cs
@@ -10,7 +10,7 @@ namespace SchoolManagementSystem.Controllers {
         }
 
         public IActionResult Index() { 
-            return View();
+            return View(roleManager.Roles);
         }
 
         private void Errors(IdentityResult result) {
