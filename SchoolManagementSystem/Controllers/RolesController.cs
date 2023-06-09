b7a823d5a868d16021b77947035422fa04a0de79
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-06-09 21:45:49 +0200

    Add "delete" method to Roles Also, change headings in couple of views

--- SchoolManagementSystem/Controllers/RolesController.cs
+++ SchoolManagementSystem/Controllers/RolesController.cs
@@ -32,6 +32,21 @@ namespace SchoolManagementSystem.Controllers {
             return View(name);      //jestlize "name" neni Valid, tak se pouze zobrazi znova View, kde je name vypsany
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id) {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null) {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "No role found");
+            return View("Index", roleManager.Roles);
+        }
+
         private void Errors(IdentityResult result) {
             foreach (IdentityError error in result.Errors)
                 ModelState.AddModelError("", error.Description);
