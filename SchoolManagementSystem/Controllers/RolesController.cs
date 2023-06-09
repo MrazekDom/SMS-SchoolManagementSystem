0395971d0e89e46c307cf6bf04fc1a28f3a5a6b0
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-06-09 21:15:59 +0200

    Change home and login views Add a picture to login view, make both pictures responsive, change login text

--- SchoolManagementSystem/Controllers/RolesController.cs
+++ SchoolManagementSystem/Controllers/RolesController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SchoolManagementSystem.Controllers {
+    [Authorize]
     public class RolesController : Controller {
         private RoleManager<IdentityRole> roleManager;
 
--- SchoolManagementSystem/ViewModels/LoginVM.cs
+++ SchoolManagementSystem/ViewModels/LoginVM.cs
@@ -3,6 +3,7 @@
 namespace SchoolManagementSystem.ViewModels {
     public class LoginVM {
         [Required]
+        [Display(Name ="User name")]
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
