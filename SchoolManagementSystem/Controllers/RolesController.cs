7281d62b63d702ec95b6c76ee2aa0b0785fef979
Dominik Mrázek <mrazekdominik@centrum.cz>
2023-06-09 21:27:47 +0200

    Add "Create" view and methods for "Roles"

--- SchoolManagementSystem/Controllers/RolesController.cs
+++ SchoolManagementSystem/Controllers/RolesController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolManagementSystem.Controllers {
     [Authorize]
     public class RolesController : Controller {
-        private RoleManager<IdentityRole> roleManager;
+        private RoleManager<IdentityRole> roleManager;      //in-built servicka
 
         public RolesController(RoleManager<IdentityRole> roleManager) {
             this.roleManager = roleManager;
@@ -13,6 +14,22 @@ namespace SchoolManagementSystem.Controllers {
 
         public IActionResult Index() { 
             return View(roleManager.Roles);
+        }
+        [HttpGet] //pouze vraci View
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name) {
+            if (ModelState.IsValid) {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            return View(name);      //jestlize "name" neni Valid, tak se pouze zobrazi znova View, kde je name vypsany
         }
 
         private void Errors(IdentityResult result) {
